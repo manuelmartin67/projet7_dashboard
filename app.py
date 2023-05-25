6260f003038d9c17ab484f951101cbf5f7edb1c9
Manuel Martin <manuelmartin67@gmail.com>
2023-05-25 09:28:56 +0200

    ajout zone de modélisation fixe

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ result = st.header(header)
 
 # Bouton pour exécuter la classification avec l'ID client sélectionné
 bouton_modelisation = st.sidebar.button("Exécuter la modélisation")
-if bouton_modelisation.button("Exécuter la modélisation"):
+if bouton_modelisation:
     # Appeler votre API Flask avec l'ID client sélectionné pour effectuer la modélisation
     url = f"https://projet7-flask.herokuapp.com/api/v1/model/id_clients?SK_ID_CURR={selected_id}"
     response = requests.get(url)
